bd1d84a8e692d65b372d6339d768c20eb719fc2f
git <duane.nielsen.rocks@gmail.com>
2018-05-16 20:53:21 -0700

    Config file done

--- apm_event_poller.py
+++ apm_event_poller.py
@@ -2,12 +2,12 @@
 
 # easy_install requests
 
-import exitstatus
 import requests
 import time
 import logging
 import json
-from snow import ServiceNow
+import snow
+
 from alertmanager import IncidentManager
 
 logging.basicConfig(filename='event_logger',
@@ -21,61 +21,77 @@ event_logger = logging.getLogger('apm_events')
 
 # Set the request parameters
 
-url = 'https://955055.apm.cloud.ca.com:443/apm/appmap/private/graph/recentstatuschanges'
+
 #url =  'https://954976.apm.cloud.ca.com:443/apm/appmap/private/graph/recentstatuschanges'
 
 #proxyKey created from APM security panel
 
-proxyKey = "40c94f7a-ab83-4d5f-92b1-0bf8a6050da8"
+
 
 #proxyKey = '858d4d13-d71a-48e5-9fbf-a2081527ff5b'
 
 # Eg. Bearer token, created in APM Notifications panel
 
-bearer = 'ffe14126-0493-4af8-8032-3a2626048a8e'
+
 
 #bearer = '7462cf42-9bbb-4ca1-8f8f-fa1ae00ddebb'
 
 # Set proper headers
 
-headers = {"Content-Type": "application/json", "Accept": "application/json", "Authorization": "Bearer " + bearer}
 
-snow = ServiceNow(hostname='dev28070.service-now.com', user='admin', pwd='Bharath@100')
-im = IncidentManager(snow, 'https://cloud.ca.com')
 
-version = 0
+
 
 #am = IncidentManager()
 
-while True:
-
-    # Do the HTTP request
-
-    response = requests.post(url, headers=headers, json={ "lastVersion" : str(version), "proxyKey" : proxyKey } )
-
-    # Check for HTTP codes other than 200
-
-    if response.status_code != 200:
-        print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.json())
-    else:
-        data = response.json()
-        im.tick(data)
-
-        # Decode the JSON response into a dictionary and use the data
+class APMEventPoller:
+    def __init__(self, host, bearer, proxyKey, incident_manger):
+        self.host = host
+        self.bearer = bearer
+        self.proxyKey = proxyKey
+        self.incident_manager = incident_manger
+        self.url = 'https://' + host + ':443/apm/appmap/private/graph/recentstatuschanges'
+        self.headers = {"Content-Type": "application/json", "Accept": "application/json", "Authorization": "Bearer " + self.bearer}
 
 
-        version = data['version']
+    def run(self):
 
-        pretty_json = json.dumps(data, indent=4, sort_keys=True)
-        event_logger.debug(pretty_json)
-        print(pretty_json)
+        version = 0
 
-        summary = []
-        matched_alerts = []
+        while True:
 
-        pretty_json = json.dumps(summary, indent=4, sort_keys=True)
-        event_logger.info(pretty_json)
-        print(pretty_json)
+            # Do the HTTP request
+
+            response = requests.post(self.url, headers=self.headers, json={ "lastVersion" : str(version), "proxyKey" : self.proxyKey } )
+
+            # Check for HTTP codes other than 200
+
+            if response.status_code != 200:
+                print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.json())
 
 
-    time.sleep(10)
+            # Decode the JSON response into a dictionary and use the data
+
+            data = response.json()
+            version = data['version']
+
+            self.incident_manager.tick(data)
+
+            pretty_json = json.dumps(data, indent=4, sort_keys=True)
+            event_logger.debug(pretty_json)
+            print(pretty_json)
+
+
+            time.sleep(2)
+
+if __name__ == '__main__':
+
+    host = '955055.apm.cloud.ca.com'
+    bearer = 'ffe14126-0493-4af8-8032-3a2626048a8e'
+    proxyKey = "40c94f7a-ab83-4d5f-92b1-0bf8a6050da8"
+
+    snow = snow.ServiceNow(hostname='dev28070.service-now.com', user='admin', pwd='Bharath@100')
+    im = IncidentManager(snow, 'https://cloud.ca.com')
+
+    apmpoller = APMEventPoller(host=host, bearer=bearer, proxyKey=proxyKey, incident_manger=im)
+    apmpoller.run()
---
+++ run.py
@@ -0,0 +1,44 @@
+from snow import ServiceNow
+from alertmanager import IncidentManager
+from apm_event_poller import APMEventPoller
+import yaml
+from pprint import pprint
+
+class Config:
+    def __init__(self):
+
+        self.apm_host = '955055.apm.cloud.ca.com'
+        self.apm_bearer_token = 'ffe14126-0493-4af8-8032-3a2626048a8e'
+        self.apm_proxyKey = "40c94f7a-ab83-4d5f-92b1-0bf8a6050da8"
+        self.apm_url = 'https://cloud.ca.com'
+
+        self.snow_hostname = 'dev28070.service-now.com'
+        self.snow_user = 'admin'
+        self.snow_pwd = 'Bharath@100'
+
+    def load(self, filename):
+        with open('config.json') as f:
+            data = yaml.load(f)
+
+        self.apm_host = data['apm_host']
+        self.apm_bearer_token = data['apm_bearer_token']
+        self.apm_proxyKey = data['apm_proxyKey']
+        self.apm_url = data['apm_url']
+
+        self.snow_hostname = data['snow_hostname']
+        self.snow_user = data['snow_hostname']
+        self.snow_pwd = data['snow_pwd']
+
+        pprint(data)
+
+    def getPoller(self):
+        snow = ServiceNow(hostname=self.snow_hostname, user=self.snow_user, pwd=self.snow_pwd)
+        im = IncidentManager(snow, self.apm_url)
+        return APMEventPoller(host=self.apm_host, bearer=self.apm_bearer_token, proxyKey=self.apm_proxyKey, incident_manger=im)
+
+
+if __name__ == '__main__':
+    config = Config()
+    config.load('config.json')
+    apmPoller = config.getPoller()
+    apmPoller.run()
