c2a116a6d0d85ebe6c4394a05202d25c5e19834a
git <duane.nielsen.rocks@gmail.com>
2018-05-17 09:36:27 -0700

    Added cooldown to config

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ServiceNowIntegration',
+    version='0.1',
+    packages=['requests','PyYAML'],
+    url='https://github.com/DuaneNielsen/ServiceNowIntegration',
+    license='Apache 2.0',
+    author='niedu02',
+    author_email='',
+    description='Integration to ServiceNow'
+)
